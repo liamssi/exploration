fdb283e568b88f6bd6ddc2b1ef2ef3a64fa0eda1
Belghit Ismail <liamssi.dz@gmail.com>
2021-02-07 21:02:52 +0100

    base template setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Counter from '$components/Counter.svelte';
+</script>
+
+<main>
+	<h1>Hello world!</h1>
+
+	<Counter/>
+	<p>Visit the <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style >
+
+	
+</style>
